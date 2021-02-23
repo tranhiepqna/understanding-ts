f30c503b281f7f05ba025ea193cfe0be30ed50f6
tranhiepqna <tranhiepqna@gmail.com>
2021-02-24 00:10:53 +0700

    Including and Excluding files

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-function add(n1, n2) {
-    return n1 + n2;
-}
-function printResult(num) {
-    console.log("Result: ", num);
-}
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
-var combineValues;
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5; 
-console.log(combineValues(5, 12));
-addAndHandle(10, 20, function (num) {
-    console.log(num);
-    return true;
-});
+"use strict";
+var userNameTS = 'Max';
+console.log(userNameTS);
---
+++ app.ts
@@ -0,0 +1,3 @@
+const userNameTS = 'Max';
+
+console.log(userNameTS)
