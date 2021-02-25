cc258c99e7a364b9377d693877fc321910370348
tranhiepqna <tranhiepqna@gmail.com>
2021-02-25 23:24:26 +0700

    add let-const.ts

---
+++ src/let-const.ts
@@ -0,0 +1,13 @@
+const userName = 'Max';
+
+let age = 30;
+
+age = 29;
+
+// function add(a : number, b: number){
+//     let result;
+//     result = a + b;
+//     return result;
+// }
+
+const add = (a: number, b: number) => a + b; 
